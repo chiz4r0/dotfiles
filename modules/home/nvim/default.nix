62653990ebc4fa021aed2150bf23d262f1a7aeb0
chiz4r0 <yurandarezky@gmail.com>
2025-07-07 18:29:01 +0700

    add lazyvim

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -2,11 +2,17 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [ ripgrep fd ];
+  home.packages = with pkgs; [ ripgrep fd gcc lua luarocks ];
 
+  xdg.configFile."nvim/init.lua".source = ./lazy/init.lua;
 
-#  xdg.configFile."nvim/lua/plugins/example.lua".source = ./config/lua/plugins/colorscheme.lua;
-  xdg.configFile."nvim/init.lua".source = ./config/init.lua;
+  xdg.configFile."nvim/lua/config/lazy.lua".source = ./lazy/lua/config/lazy.lua;
+  xdg.configFile."nvim/lua/config/keymaps.lua".source = ./lazy/lua/config/keymaps.lua;
+  xdg.configFile."nvim/lua/config/options.lua".source = ./lazy/lua/config/options.lua;
+  xdg.configFile."nvim/lua/config/autocmds.lua".source = ./lazy/lua/config/autocmds.lua;
+
+  xdg.configFile."nvim/lua/plugins/example.lua".source = ./lazy/lua/plugins/example.lua;
+
   programs.neovim = {
     enable = true;
     vimAlias = true;
