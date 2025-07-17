3e669d19b2a42da563aac811e5320696482d4acc
chiz4r0 <yurandarezky@gmail.com>
2025-07-17 17:46:18 +0700

    add telescope

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -3,6 +3,10 @@
   pkgs,
   ...
 }: {
+  imports = [
+    ./plugins/telescope.nix
+  ];
+
   programs.nvf = {
     enable = true;
     settings = {
@@ -96,54 +100,6 @@
         ui.noice.enable = true;
         ui.borders.plugins.which-key.style = "solid";
         visuals.nvim-web-devicons.enable = true;
-
-        telescope = {
-          enable = true;
-
-          extensions = [
-            {
-              name = "fzf";
-              packages = [pkgs.vimPlugins.telescope-fzf-native-nvim];
-              setup = {fzf = {fuzzy = true;};};
-            }
-            {
-              name = "file_browser";
-              packages = [pkgs.vimPlugins.telescope-file-browser-nvim];
-              setup = {
-                file_browser = {
-                  path = "%:p:h";
-                  cwd = lib.literalExpression "function() return vim.fn.expand('%:p:h') end";
-                  respect_gitignore = false;
-                  hidden = true;
-                  grouped = true;
-                  previewer = false;
-                  initial_mode = "normal";
-                  layout_config = {
-                    height = 40;
-                  };
-                  hijack_netrw = true;
-                };
-              };
-            }
-          ];
-        };
-        luaConfigRC.telescope = ''
-          require('telescope').setup{
-            defaults = {
-              wrap_results = true,
-              layout_strategy = 'horizontal',
-              layout_config = {
-                prompt_position = 'top',
-                preview_width = 0.55,
-                width = 0.9,
-                height = 0.85,
-              },
-              sorting_strategy = "ascending",
-              winblend = 15,
-              file_ignore_patterns = { "node_modules", ".git/" }
-            }
-          }
-        '';
 
         luaConfigRC.options = ''
           vim.cmd [[
